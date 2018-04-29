881f5d0f0a430bb0c1685a834ce80423c373c719
Administratör <Administratr@5CG7065BJS>
2018-04-30 01:49:00 +0200

    Add project files.

---
+++ WebApplication2/Controllers/MessagesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Web.WebSockets;
+
+namespace uppgift_3.Controllers
+{
+    public class MessagesController : ApiController
+    {
+
+        public HttpResponseMessage Get(String username)
+        {
+            //accepts the web socket request and sets up a class called ChatwsHandler to manage the connection- username parameter for each new connction.
+            HttpContext.Current.AcceptWebSocketRequest(new ChatwsHandler(username));
+
+            //sends back a response with HTTP status code 101 telling the client that it has agreed to switch to the WebSocket protocol.
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+       
+
+        class ChatwsHandler : WebSocketHandler
+        {
+            //this class manages a list of chat clients and broadcasts the messages to all clients when it recives a message.
+
+            private static WebSocketCollection chatClients = new WebSocketCollection();
+            private string enteredUsername;
+
+            public ChatwsHandler(string username)
+            {
+                //sets entered username to the username parameter that will be sending and recieving the messages
+
+                enteredUsername = username;
+            }
+
+            public override void OnOpen()
+            {
+                //'override' the normal OnOpen fuction that happens in WebSocketHandler to the code below.
+                //have the new chat clients (usernames entered) added to the websocketcollection as they connect.
+
+                chatClients.Add(this);
+
+            }
+
+            public override void OnMessage(string message)
+            {
+                //'override' the normal OnMessage fuction that happens in WebSocketHandler to the code below.
+                //to format what will be pushed from the server to all the clients connected.
+
+                chatClients.Broadcast(enteredUsername + ": " + message);
+            }
+        }
+
+    }
+}
---
+++ WebApplication2/JSuppgift_3.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    var websocket;
+
+    $('#userConnectionForm').on('submit', function (e) {
+        e.preventDefault();
+        var username = $('#userNameToBeEnteredBox').val();
+
+        var uri = 'ws://localhost:60393/api/Messages' + '?username=' + username;
+        websocket = new WebSocket(uri);
+
+        $('#userConnectionForm').hide();
+         $('#sendButton').on('click', function () {
+        websocket.send($('#inputbox').val());
+    });
+    
+
+    websocket.onopen = function () {
+        $('#messages').prepend('Connected.');
+    };
+
+    websocket.onerror = function (event) {
+        //if the websocket cannot connect
+        $('#messages').prepend('ERROR!');
+    };
+
+    websocket.onmessage = function (event) {
+        $('#messages').prepend('<div>' + event.data + '</div>');
+    };
+    });
+
+});
